f1d1a2324133b11ffdcf602e67a60039c8e67e22
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-08 16:38:07 +0100

    Modifiche js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const hamMenu = document.querySelector('.fas.fa-bars');
+const hamMenuNext = document.querySelector('.fas.fa-times');
+let menu = document.querySelector('.hamburger-menu');
 
 hamMenu.addEventListener('click', function() {
 
-    let menu = document.querySelector('.hamburger-menu');
-    menu.style.display = 'block' ;
+   menu.style.display = 'block' ;
 
 })
 
-const hamMenuNext = document.querySelector('.fas.fa-times');
-
 hamMenuNext.addEventListener('click', function() {
 
-    menu = document.querySelector('.hamburger-menu');
-    menu.style.display = 'none' ;
+   menu.style.display = 'none' ;
 
 })
